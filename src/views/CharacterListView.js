10f3842273df93ff52ecf1984e31df6cd996ec8d
Oliver-Strange <preston.burton@gmail.com>
2019-05-22 15:47:25 -0700

    fixed reference to actions

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -4,18 +4,23 @@ import { connect } from "react-redux";
 import { CharacterList } from "../components";
 // import actions
 
+import { getCharacters } from '../actions/index';
+
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
     // call our action
+    console.log('cdm happened');
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <h2>Finding Characters....</h2>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +32,18 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error,
+    isFetching: state.charsReducer.isFetching
+  }
+}
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
